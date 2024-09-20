92b4e298a44f9891749cac8c580df3d17d33c905
Enoakpo Dennis <codetitan2206@gmail.com>
2024-09-20 13:23:20 +0100

    added a verify email routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const userController = require("../controllers/userController");
 const authController = require("../middleware/authController");
+const verifyEmailController = require("../middleware/verifyemail");
 
 const router = express.Router();
 router.route("/signUp").post(authController.signUp);
 router.route("/login").post(userController.login);
+router.route("/verifyEmail/:token").post(authController.verifyEmail);
 module.exports = router;

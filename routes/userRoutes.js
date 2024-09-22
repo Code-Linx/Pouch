267afd0013a66b2e4f134368593635a9efff62f8
Enoakpo Dennis <codetitan2206@gmail.com>
2024-09-22 20:56:44 +0100

    updated the route with forget and reset password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,24 @@
-const express = require("express");
-
-const userController = require("../controllers/userController");
-const authController = require("../middleware/authController");
-const verifyEmailController = require("../middleware/verifyemail");
+const express = require('express');
+const userController = require('../controllers/userController');
+const authController = require('../middleware/authController');
+const verifyEmailController = require('../middleware/verifyemail');
 
 const router = express.Router();
-router.route("/signUp").post(authController.signUp);
-router.route("/login").post(authController.login);
-router.route("/verifyEmail/:token").post(verifyEmailController.verifyEmail);
+
+router.post('/register', authController.signUp); // Fix this line
+router.post('/login', authController.login);
+router.post('/logout', authController.logout);
+router.post('/verifyEmail/:token', verifyEmailController.verifyEmail);
+
+router.post('/forgetPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+//PROTECTED ROUTES
+
+router.patch(
+  '/updateMypassword',
+  authController.protect,
+  authController.updatePassword
+);
+
 module.exports = router;

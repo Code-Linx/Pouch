77903f8cfb7fee5b19b2f8717f957c95e2c6f4f1
Enoakpo Dennis <codetitan2206@gmail.com>
2024-09-23 17:14:36 +0100

    updated the user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,28 @@
-exports.login = (req, res, next) => {
-  res.status(200).json({
-    status: "success",
-    message: "This route is not yet defined",
-  });
+const User = require('../model/userModel');
+
+exports.getMe = async (req, res, next) => {
+  try {
+    // Fetch the current user by ID
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'User not found',
+      });
+    }
+
+    // Respond with user profile data
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user, // User profile information
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
 };

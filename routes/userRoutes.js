0f5592541b071153a2ac8444de715b0d6b01b892
Enoakpo Dennis <codetitan2206@gmail.com>
2024-09-25 15:11:43 +0100

    updated user route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,8 @@ const verifyEmailController = require('../middleware/verifyemail');
 const budgetController = require('../controllers/budgetController');
 const dashboardController = require('../controllers/dashboardController');
 const categoryController = require('../controllers/categoryController');
+const transController = require('../controllers/TransController');
+const { route } = require('./adminRoutes');
 const router = express.Router();
 
 router.post('/register', authController.signUp); // Fix this line
@@ -24,10 +26,17 @@ router.patch(
 );
 
 router.get(
+  '/getAllBudget',
+  authController.protect,
+  budgetController.getAllMyBudget
+);
+
+router.get(
   '/dashboard',
   authController.protect,
   dashboardController.getDashboard
 );
+
 router.post(
   '/addbudgets',
   authController.protect,
@@ -41,5 +50,33 @@ router.post(
 ); // Create category
 
 router.get('/me', authController.protect, userController.getMe);
+
+router.patch(
+  '/editBudget/:id',
+  authController.protect,
+  authController.restrictTo('user'),
+  budgetController.updateBudget
+);
+
+router.delete(
+  '/removeBudget/:id',
+  authController.protect,
+  authController.restrictTo('user'),
+  budgetController.deleteBudget
+);
+
+router.delete(
+  '/deleteTrx/:id',
+  authController.protect,
+  authController.restrictTo('user'),
+  transController.deleteTrans
+);
+
+router.patch(
+  '/updateTrx/:id',
+  authController.protect,
+  authController.restrictTo('user'),
+  transController.updateTrans
+);
 
 module.exports = router;

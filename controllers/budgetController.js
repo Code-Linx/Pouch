117723e5658c5e24a43ad9e4c8e21ef52b1ac302
Enoakpo Dennis <codetitan2206@gmail.com>
2024-09-25 15:09:01 +0100

    added function for admin to edit and delete category

--- controllers/budgetController.js
+++ controllers/budgetController.js
@@ -1,5 +1,6 @@
 const Budget = require('../model/budgetModel');
 const Category = require('../model/categoryModel');
+
 exports.createBudget = async (req, res) => {
   try {
     const categoryName = req.body.category; // Expecting category name
@@ -25,6 +26,78 @@ exports.createBudget = async (req, res) => {
       data: {
         budget,
       },
+    });
+  } catch (error) {
+    res.status(400).json({ status: 'error', message: error.message });
+  }
+};
+
+exports.getAllMyBudget = async (req, res, next) => {
+  try {
+    const budget = await Budget.find({ userId: req.user._id });
+    if (!budget) {
+      return res.status(200).json({
+        status: 'success',
+        message: 'You do not have a budget Yet, Please create One',
+      });
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        budget,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ status: 'error', message: error.message });
+  }
+};
+
+exports.updateBudget = async (req, res, next) => {
+  try {
+    const { monthlyBudget, monthlyIncome, monthlyExpenses } = req.body.id;
+    const budget = await Budget.findByIdAndUpdate(
+      req.params.id,
+      {
+        monthlyBudget,
+        monthlyIncome,
+        monthlyExpenses,
+      },
+      {
+        new: true,
+      }
+    );
+
+    if (!budget) {
+      return res.status(400).json({
+        status: 'Failed',
+        message: 'No budget with that Id',
+      });
+    }
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        budget,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ status: 'error', message: error.message });
+  }
+};
+
+exports.deleteBudget = async (req, res, next) => {
+  try {
+    const budget = await Budget.findByIdAndDelete(req.params.id);
+    if (!budget) {
+      return res
+        .status(400)
+        .json({ status: 'error', message: 'Budget not found' });
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
     });
   } catch (error) {
     res.status(400).json({ status: 'error', message: error.message });
--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,6 @@
 const Category = require('../model/categoryModel');
 
-exports.createCategory = async (req, res) => {
+exports.createCategory = async (req, res, next) => {
   try {
     const { name } = req.body; // Expecting { "name": "Groceries" }
 
@@ -11,6 +11,56 @@ exports.createCategory = async (req, res) => {
       data: {
         category,
       },
+    });
+  } catch (error) {
+    res.status(400).json({ status: 'error', message: error.message });
+  }
+};
+
+exports.deleteCategory = async (req, res, next) => {
+  try {
+    const { id } = req.params.id;
+    const category = await Category.findByIdAndDelete(id);
+    if (!category) {
+      return res.status(404).json({
+        status: 'Failed',
+        message: 'No doc with that id',
+      });
+    }
+
+    res.status(204).json({
+      status: 'Success',
+      data: null,
+    });
+  } catch (error) {
+    res.status(400).json({ status: 'error', message: error.message });
+  }
+};
+
+exports.getAllcategory = async (req, res, next) => {
+  try {
+    const category = await Category.find();
+    res.status(200).json({
+      status: 'Success',
+      data: category,
+    });
+  } catch (error) {
+    res.status(400).json({ status: 'error', message: error.message });
+  }
+};
+
+exports.editCategory = async (req, res, next) => {
+  try {
+    const category = await Category.findByIdAndUpdate(req.params.id);
+    if (!category) {
+      res.status(404).json({
+        status: 'Failed',
+        message: 'No category with that ID',
+      });
+    }
+    res.status(201).json({
+      status: 'Success',
+      data: category,
     });
   } catch (error) {
     res.status(400).json({ status: 'error', message: error.message });
